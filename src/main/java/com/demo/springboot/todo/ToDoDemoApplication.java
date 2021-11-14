28cbde1de3084f9a68daf33b059a20332b7d6f84
unknown <dipan.mukherjee1990@gmail.com>
2021-11-14 12:52:52 +0530

    Initial commit

---
+++ src/main/java/com/demo/springboot/todo/ToDoDemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.springboot.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoDemoApplication.class, args);
+	}
+}
